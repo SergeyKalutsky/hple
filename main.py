cd7a3e35953a958fe0d23df712eb2dc0abffe6e5
SergeyKalutsky <skalutsky@gmail.com>
2022-01-01 22:05:50 +0300

    learned how to get a user object

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ bot = commands.Bot(command_prefix='$')
 async def ping(ctx):
     await ctx.send('hello world')
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    # message.author.id <-- user_id
+    res = await bot.fetch_user(message.author.id)
+    print(res)
+    await message.channel.send('Hello!!!!!')
+
 bot.run(TOKEN)
