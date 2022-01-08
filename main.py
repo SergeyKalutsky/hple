68f8b7906779ff323162965073a82a8e378a4c4a
SergeyKalutsky <skalutsky@gmail.com>
2022-01-08 22:09:07 +0300

    muting works

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 from api_token import TOKEN
-bot = commands.Bot(command_prefix='$')
+
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 
 @bot.command('test')
 async def ping(ctx):
     await ctx.send('testing')
 
+
+async def mute_member(channels, member):
+    for channel in channels:
+        for voice_member in channel.members:
+            if member == voice_member:
+                await member.edit(mute=True)
+                return
 
 # @bot.event
 # async def on_message(message):
@@ -18,6 +29,7 @@ async def ping(ctx):
 #    res = await bot.fetch_user(message.author.id)
 #    print(res)
 #    await message.channel.send('Hello!!!!!')
+
 
 @bot.command('unmute')
 async def mute(ctx, member: discord.Member = None):
@@ -37,8 +49,14 @@ async def mute(ctx, member: discord.Member = None):
         muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
         await member.add_roles(muted_role)
         await member.send(f"You have been muted")
+        await mute_member(ctx.guild.voice_channels, member)
     else:
         await ctx.send("Didn't work, try using the command *properly*")
 
+
+@bot.command('muteall')
+async def muteall(ctx):
+    for channel in ctx.guild.voice_channels:
+        print(channel.members)
 
 bot.run(TOKEN)
