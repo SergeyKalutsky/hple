044574d4db2c7937dcf13be42d77eaff8b353e2a
SergeyKalutsky <skalutsky@gmail.com>
2022-01-22 21:25:52 +0300

    select offence works

--- db/db.py
+++ db/db.py
@@ -10,4 +10,11 @@ def log_message(message, author):
     cur = con.cursor()
     cur.execute(f"INSERT INTO logs VALUES ('{message}', '{date}', '{author}')")
     con.commit()
-    
+
+
+def select_offence_count(username):
+    query = f'''select count(*) from logs
+                where user = '{username}' '''
+    cur = con.cursor()
+    cur.execute(query)
+    return cur.fetchone()[0]
