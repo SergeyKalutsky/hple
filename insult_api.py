7351050a7b28ebc463f4957f1608c6916ab92fc6
SergeyKalutsky <skalutsky@gmail.com>
2022-02-25 14:54:35 +0300

    changes

--- insult_api.py
+++ insult_api.py
@@ -3,7 +3,5 @@ import requests
 
 def is_insult(text):
     # It only works when docker is runnig on a localhost
-    res = requests.post('http://127.0.0.1:5555/model', data=json.dumps({"x": [text]}))
+    res = requests.post('https://7006.lnsigo.mipt.ru/model', data=json.dumps({"x": [text]}))
     return  res.json()[0][0]
-
-is_insult('hello')
